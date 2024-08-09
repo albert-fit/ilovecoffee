89fa5475969441e3ba0f9f130c10d116cfe3acbb
Albert Fit <af@albert-fit.com>
2024-08-09 14:45:54 +0100

    added development and production classes.

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,11 +4,34 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entitiy';
+import { COFFEE_BRANDS } from './coffees.constants';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    /* ... do something ... */
+    return ['buddy brew', 'nescafe'];
+  }
+}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    CoffeeBrandsFactory,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
+    { provide: COFFEE_BRANDS, useFactory: () => ['buddy brew', 'nescafe'] },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entitiy';
 import { Event } from 'src/events/entities/event.entitiy';
@@ -7,6 +7,7 @@ import { DataSource, Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Injectable()
 export class CoffeesService {
@@ -16,7 +17,10 @@ export class CoffeesService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
     private readonly dataSource: DataSource,
-  ) {}
+    @Inject(COFFEE_BRANDS) coffeeBrands: string[],
+  ) {
+    console.log(coffeeBrands);
+  }
 
   async findAll(paginationQuery: PaginationQueryDto) {
     const { limit, offset } = paginationQuery;
