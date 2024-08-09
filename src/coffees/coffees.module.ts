9d3efc24a125038a698ed1da4fd78fa4a9dc068f
Albert Fit <af@albert-fit.com>
2024-08-09 15:06:48 +0100

    created a dynamic database module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entitiy';
 import { COFFEE_BRANDS } from './coffees.constants';
+import { Connection } from 'typeorm';
 
 class ConfigService {}
 
@@ -25,7 +26,15 @@ export class CoffeeBrandsFactory {
       provide: ConfigService,
       useFactory: () => ['buddy brew', 'nescafe'],
     },
-    { provide: COFFEE_BRANDS, useFactory: () => ['buddy brew', 'nescafe'] },
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('Factory');
+        return coffeeBrands;
+      },
+      inject: [Connection],
+    },
   ],
   exports: [CoffeesService],
 })
